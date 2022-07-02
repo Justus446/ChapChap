5948a912ae7b178dad03c5dd87fbaf4c5a77d3ff
Justus Omondi <85983194+Justus446@users.noreply.github.com>
2022-07-02 22:57:31 +0300

    Single product page

--- webapp/calc/models.py
+++ webapp/calc/models.py
@@ -4,11 +4,15 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     img = models.ImageField(upload_to='pics')
+    name = models.TextField()
+    location = models.CharField(max_length=30)
     desc = models.TextField()
     price = models.IntegerField()
 
 
 class Item(models.Model):
     img = models.ImageField(upload_to='pics')
+    name = models.TextField()
+    location = models.CharField(max_length=30)
     desc = models.TextField()
     price = models.IntegerField()
