b07ee9a4b58e4bdf408bfdd243508928ed5e7669
Justus Omondi <85983194+Justus446@users.noreply.github.com>
2022-07-12 23:25:16 +0300

    API complete

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from  django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.urlpatterns import format_suffix_patterns
+from api import views
 
 
 urlpatterns = [
     path('', include('calc.urls')),
     path('admin/', admin.site.urls),
-    # path('/api', include('api.urls')),
+    path('api/', views.productList.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
